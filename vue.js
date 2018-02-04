ad2f2830fdf63c4f9104ebae38da08158f313de1
José Rafael Aguilera <joserafaelaguilera@gmail.com>
2018-02-04 17:15:31 +0100

    Mostrar lista de tareas

---
+++ vue.js
@@ -0,0 +1,23 @@
+new Vue({
+	el: '#app',
+	data: {
+		titulo: 'Lista de Tareas',
+		tareas: [
+			{
+				texto: 'Aprender Vue.js',
+				terminada: false
+			},
+			{
+				texto: 'Aprender Angular 2',
+				terminada: false
+			},
+			{
+				texto: 'Aprender Ionic 2',
+				terminada: false
+			},
+		]
+	},
+	method:{
+
+	}
+})
